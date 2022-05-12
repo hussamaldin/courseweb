41f0dd2a3a0475017122e1d02c8e479286047504
Your Name <you@example.com>
2022-05-12 23:38:44 +0200

    sosl

--- src/components/DataDriven.vue
+++ src/components/DataDriven.vue
@@ -3,19 +3,19 @@
                 <div class="w-full text-white flex flex-row sm:flex-wrap overflow-hidden h-[500px] bg-gray-700">
         <div class="sm:w-full xl:w-[50%] h-full sm:h-auto flex justify-center sm:justify-start sm:absolute sm:top-[250px] items-center"><div class="text-5xl ph:text-xl sm:text-3xl">Data driven investment process</div></div>
         <div class="sm:w-full xl:w-[50%] h-full sm:h-auto relative">
-          <svg class="absolute z-0 top-[130px] sm:top-[300px] ph:top-[300px] right-[80px] sm:left-[150px]" width="100" height="200" viewBox="-1 -1 240 300">
+          <svg class="absolute z-0 xl:top-[130px] sm:top-[300px] ph:top-[300px] xl:right-[80px] sm:left-[150px]" width="100" height="200" viewBox="-1 -1 240 300">
   <path d="M104,0 L208,60 L208,180 L104,240 L0,180 L0,60z" stroke="black" stroke-width="1" fill="transparent" />
 </svg>
-<svg class="absolute bottom-[10px] ph:top-[350px] sm:top-[350px] z-0 right-[100px] ph:left-[-200px] sm:left-[100px]" width="320" height="320" viewBox="-1 -1 240 300">
+<svg class="absolute xl:bottom-[10px] ph:top-[350px] sm:top-[350px] z-0 xl:right-[100px] ph:left-[-200px] sm:left-[100px]" width="320" height="320" viewBox="-1 -1 240 300">
   <path d="M104,0 L208,60 L208,180 L104,240 L0,180 L0,60z" stroke="orange" stroke-width="2" fill="transparent" />
 </svg>
         </div>
     </div>
     <!--pagecontent-->
 <div>
-                  <div class="w-full h-[450px] ph:h-auto sm:h-auto bg-gray-200 p-4 flex flex-row ph:flex-wrap sm:flex-wrap">
-        <div class="text-4xl ph:text-xl ph:w-full sm:w-full xl:w-[50%] p-4 text-gray-600 opacity-0 slidet">We use a number of key performance indicators (KPIs) to assess progress against our strategic objectives, both financial and non-financial</div>
-        <div class="sm:w-full ph:text-sm ph:w-full xl:w-[50%] text-xl text-gray-600 p-4 opacity-0 slidet leading-8">Our team’s background in basic science, clinical development and commercialisation enables a holistic understanding of the opportunity, from the scientific basis through to the commercial opportunity. More specifically, the Syncona team conducts a rigorous returns analysis for existing investments ahead of each portfolio company financing round, and re-evaluates the original investment thesis.
+                  <div class="w-full xl:h-[450px] ph:h-auto sm:h-auto bg-gray-200 p-4 flex flex-row ph:flex-wrap sm:flex-wrap">
+        <div class="sm:text-4xl ph:text-xl ph:w-full sm:w-full xl:w-[50%] p-4 text-gray-600 opacity-0 slidet">We use a number of key performance indicators (KPIs) to assess progress against our strategic objectives, both financial and non-financial</div>
+        <div class="sm:w-full ph:text-sm ph:w-full xl:w-[50%] sm:text-xl text-gray-600 p-4 opacity-0 slidet leading-8">Our team’s background in basic science, clinical development and commercialisation enables a holistic understanding of the opportunity, from the scientific basis through to the commercial opportunity. More specifically, the Syncona team conducts a rigorous returns analysis for existing investments ahead of each portfolio company financing round, and re-evaluates the original investment thesis.
 
 We have a hands-on approach, often taking operational roles, and work in partnership with the management teams of our companies to deliver the best results.</div>
         
@@ -23,31 +23,31 @@ We have a hands-on approach, often taking operational roles, and work in partner
 
 <div class="flex justify-center p-4">
     <div class="w-[80%]">
-        <div class="text-gray-700 text-4xl ph:text-xl font-bold opacity-0 slidet my-4">Proactive search by Syncona team for appropriate assets</div>
-        <div class="text-gray-700 text-4xl ph:text-xl font-bold opacity-0 slidet my-4">Ideal characteristics of a scientific asset</div>
+        <div class="text-gray-700 sm:text-4xl ph:text-xl font-bold opacity-0 slidet my-4">Proactive search by Syncona team for appropriate assets</div>
+        <div class="text-gray-700 sm:text-4xl ph:text-xl font-bold opacity-0 slidet my-4">Ideal characteristics of a scientific asset</div>
         <div class=" opacity-0 slidet"><img :src="img1" class="h-[400px] ph:h-[200px]"></div>
         <div class=" opacity-0 slidet">
-        <div class="text-gray-700 my-4 text-3xl ph:text-xl font-bold">Key decision-making factors</div>
-       <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Intellectual property</div>
-       <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Globally leading academics</div>
-       <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Technology</div>
+        <div class="text-gray-700 my-4 sm:text-3xl ph:text-xl font-bold">Key decision-making factors</div>
+       <div class="sm:text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Intellectual property</div>
+       <div class="sm:text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Globally leading academics</div>
+       <div class="ctext-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Technology</div>
        </div>
         <div  class=" opacity-0 slidet">
-               <div class="text-gray-700 ph:text-xl my-4 text-3xl font-bold">Key decision-making factors</div>
-       <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Assess probability of success for the product’s approval, taking account of benchmarks if relevant or appropriate</div>
-       <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Write business plan and develop plan to build out team and operational capability</div>
+               <div class="text-gray-700 ph:text-xl my-4 sm:text-3xl font-bold">Key decision-making factors</div>
+       <div class="sm:text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Assess probability of success for the product’s approval, taking account of benchmarks if relevant or appropriate</div>
+       <div class="sm:text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Write business plan and develop plan to build out team and operational capability</div>
  </div>
  <div class=" opacity-0 slidet">
-       <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>For each new investment, the Syncona Investment Committee assesses capital invested across the portfolio to ensure it is diversified and well-balanced</div>
-       <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Assess the long-term capital requirements and key risks and value inflection points</div>
-       <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>For each new investment, the Syncona Investment Committee assesses capital invested across the portfolio to ensure it is diversified and well-balanced</div>
-              <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Assess the long-term capital requirements and key risks and value inflection points</div>
+       <div class="sm:text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>For each new investment, the Syncona Investment Committee assesses capital invested across the portfolio to ensure it is diversified and well-balanced</div>
+       <div class="sm:text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Assess the long-term capital requirements and key risks and value inflection points</div>
+       <div class="sm:text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>For each new investment, the Syncona Investment Committee assesses capital invested across the portfolio to ensure it is diversified and well-balanced</div>
+              <div class="sm:text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Assess the long-term capital requirements and key risks and value inflection points</div>
        </div>
         <div class=" opacity-0 slidet">
                <div class="text-gray-700 ph:text-xl my-4 text-3xl font-bold">Key decision-making factors</div>
-       <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Ongoing evaluation of emerging data and analysis of evolving competitive landscape</div>
-       <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Globally leading academics</div>
-       <div class="text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Undertakes rigorous returns analysis</div>
+       <div class="sm:text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Ongoing evaluation of emerging data and analysis of evolving competitive landscape</div>
+       <div class="sm:text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Globally leading academics</div>
+       <div class="sm:text-xl ph:text-sm my-4"> <span class="text-yellow-500">&mdash;</span>Undertakes rigorous returns analysis</div>
        </div>
 </div>
 </div>
@@ -55,8 +55,8 @@ We have a hands-on approach, often taking operational roles, and work in partner
         <div class="bg-gray-700 w-full h-[500px] mt-4 p-4">
   <div class="flex justify-center text-white p-4 opacity-0 slidet text-3xl ph:text-xl"><div>RELATED CONTENT</div></div>
   <div class="flex flex-row ph:flex-wrap sm:flex-wrap justify-between p-4 h-[90%] opacity-0 slidet">
-   <div class="flex justify-center ph:w-full sm:w-full xl:w-[50%] ph:border-t sm:border-t xl:border-t-0 border-gray-300 text-white text-5xl ph:text-2xl items-center"><div><a class="hover:text-white hover:underline duration-500">KPSI</a></div></div>
-   <div class="flex justify-center ph:w-full sm:w-full xl:w-[50%] ph:border-t sm:border-t xl:border-t-0 xl:border-l border-gray-300 text-white text-5xl ph:text-2xl items-center duration-500"><div><a class="hover:text-white hover:underline duration-500">Investor</a></div></div>
+   <div class="flex justify-center ph:w-full sm:w-full xl:w-[50%] ph:border-t sm:border-t xl:border-t-0 border-gray-300 text-white ph:text-2xl xl:text-5xl ph:text-2xl items-center"><div><a class="hover:text-white hover:underline duration-500">KPSI</a></div></div>
+   <div class="flex justify-center ph:w-full sm:w-full xl:w-[50%] ph:border-t sm:border-t xl:border-t-0 xl:border-l border-gray-300 text-white  ph:text-2xl xl:text-5xl ph:text-2xl items-center duration-500"><div><a class="hover:text-white hover:underline duration-500">Investor</a></div></div>
   </div>
 </div>
 
